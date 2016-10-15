e9b5d8f19bee161247d4c89cced821126f18b6e2
Sam <sam@cwc-0178.dhcp.wustl.edu>
2016-10-14 19:34:46 -0500

    I've decided to do "flat is better than nested"

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
-# from pymath2.exceptions import *
+from pymath2.exceptions import *
 from pymath2.undefined import Undefined
 from pymath2.const import const
 from pymath2.var import var
-from pymath2.functions import unseeded_function as func
+from pymath2.functions import *
+func = unseeded_function
 __all__ = ('const', 'var', 'func')
