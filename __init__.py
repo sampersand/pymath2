410857c037fcc7d8e24f90c8a62fc2e806c900ea
Sam <sam@cwc-0178.dhcp.wustl.edu>
2016-10-17 19:20:32 -0500

    I just remembed: Make a class called CustomFunction or something that extends UnseededFunction and uses regexes to attempt to parse what name, args_str, and body str is.

--- __init__.py
+++ __init__.py
@@ -4,5 +4,6 @@ from .builtins.variable import Variable as var
 from .builtins.derivative import Derivative as d
 from .builtins.functions.unseeded_function import UnseededFunction as func
 from .extensions import *
+
 __all__ = tuple(x for x in tuple(locals()) if x[0] != '_')
 # __all__ = ('const', 'var', 'func', '')
